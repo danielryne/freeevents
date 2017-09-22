b65386936e6db70c79d819e251671320ccf8adaa
Elliott Wendel <ewendel88@gmail.com>
2017-09-21 20:01:12 -0500

    populating based on date,

--- assets/logic.js
+++ assets/logic.js
@@ -7,14 +7,17 @@ $(document).ready(function() {
         console.log("Clicked");
 
         city = "Austin";
-        startdate = "2017-10-01T00:00:00";
-        enddate = "2017-10-02T00:00:00";
+        var properTimeFormat = "YYYY-MM-DDTHH:MM:ss";
+        startDate = moment($("#startDate").val()).format(properTimeFormat); // 2017-09-23T00:00:00-05:00
+        endDate = moment($("#startDate").val()).add(1, "day").format(properTimeFormat);
+
+        console.log(moment($("#startDate").val()).format(properTimeFormat));
 
         // This is our API key
         var APIKey = "VJQ763OD7ITTFKU7NOG4";
 
         // Here we are building the URL we need to query the database
-        var queryURL = "https://www.eventbriteapi.com/v3/events/search/?price=free&location.address=" + city + "&" + "start_date.range_start=" + startdate + "Z&start_date.range_end=" + enddate + "Z&token=" + APIKey;
+        var queryURL = "https://www.eventbriteapi.com/v3/events/search/?price=free&location.address=" + city + "&" + "start_date.range_start=" + startDate + "Z&start_date.range_end=" + endDate + "Z&token=" + APIKey;
 
         // We then created an AJAX call
         $.ajax({
@@ -25,36 +28,43 @@ $(document).ready(function() {
             // Create CODE HERE to Log the queryURL
             console.log(queryURL);
 
-    //-------------------------------------------------------------------- 
+            //-------------------------------------------------------------------- 
             // Forrest's For Loop code
 
-            for (var i = 0; i < 10; i++) {
-                
+            //tells us the length of event objects 
+            eventslength = response.events.length;
+
+            for (var i = 0; i < eventslength; i++) {
+
                 // Variables for what we get
                 var nameEvent = response.events[i].name.text;
                 var urlEvent = response.events[i].url;
                 var timeEvent = response.events[i].start.local;
 
+                var date = moment(timeEvent).format("MMM Do YYYY");
+                var time = moment(timeEvent).format("h:mm:ss a");
+
                 // Clean URL (something setting urlEvent to nameEvent)
                 var cleanUrl =
 
-                // Need to seperate the timeEvent into date and time using moment
+                    // Need to seperate the timeEvent into date and time using moment
 
-                console.log(nameEvent);
+                    console.log(nameEvent);
                 console.log(timeEvent);
                 console.log(urlEvent);
 
                 $("#eventList").append(
-                    "<tr><td>"
-                        + timeEvent +
-                    "</td><td>"
-                        + urlEvent +
-                    "</td><td>"
-                        + nameEvent +
-                    "</td>")
+                    '<tr><td>' +
+                    date +
+                    '</td><td>' +
+                    time +
+                    '</td><td><a href="' + urlEvent + '">' +
+                    nameEvent +
+                    '</a></td>')
             }
 
-    //--------------------------------------------------------------------
+
+            //--------------------------------------------------------------------
             // Create CODE HERE to log the resulting object
             console.log(response);
 
