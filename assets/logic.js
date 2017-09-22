cad1ac888158df4b26dda44164ff8d8b690e9b33
Elliott Wendel <ewendel88@gmail.com>
2017-09-21 20:45:47 -0500

    gave URL categories

--- assets/logic.js
+++ assets/logic.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
         city = "Austin";
         var properTimeFormat = "YYYY-MM-DDTHH:MM:ss";
         startDate = moment($("#startDate").val()).format(properTimeFormat); // 2017-09-23T00:00:00-05:00
-        endDate = moment($("#startDate").val()).add(1, "day").format(properTimeFormat);
+        endDate = moment($("#startDate").val()).add(1, "day").format(properTimeFormat); // day after the start day
 
         console.log(moment($("#startDate").val()).format(properTimeFormat));
 
@@ -17,7 +17,7 @@ $(document).ready(function() {
         var APIKey = "VJQ763OD7ITTFKU7NOG4";
 
         // Here we are building the URL we need to query the database
-        var queryURL = "https://www.eventbriteapi.com/v3/events/search/?price=free&location.address=" + city + "&" + "start_date.range_start=" + startDate + "Z&start_date.range_end=" + endDate + "Z&token=" + APIKey;
+        var queryURL = "https://www.eventbriteapi.com/v3/events/search/?categories=110,103&price=free&location.address=" + city + "&" + "start_date.range_start=" + startDate + "Z&start_date.range_end=" + endDate + "Z&token=" + APIKey;
 
         // We then created an AJAX call
         $.ajax({
@@ -49,16 +49,12 @@ $(document).ready(function() {
 
                     // Need to seperate the timeEvent into date and time using moment
 
-                    console.log(nameEvent);
-                console.log(timeEvent);
-                console.log(urlEvent);
-
-                $("#eventList").append(
+                $("#eventList").prepend(
                     '<tr><td>' +
                     date +
                     '</td><td>' +
                     time +
-                    '</td><td><a href="' + urlEvent + '">' +
+                    '</td><td><a target="-blank" href="' + urlEvent + '">' +
                     nameEvent +
                     '</a></td>')
             }
