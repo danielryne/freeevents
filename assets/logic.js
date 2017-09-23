be572d4d4bc9123b860b4796865e796fe4870496
Forrest Eastland <feastland4@gmail.com>
2017-09-23 10:51:37 -0500

    added fullscreen.js for website look, weather api

--- assets/logic.js
+++ assets/logic.js
@@ -1,20 +1,23 @@
 $(document).ready(function() {
 
-    $("#submit").click(eventsInAustin);
+    $("button").click(eventsInAustin);
     //This is our city
     function eventsInAustin(event) {
         event.preventDefault();
         console.log("Clicked");
 
-        city = "Austin";
-        startdate = "2017-10-01T00:00:00";
-        enddate = "2017-10-02T00:00:00";
+        var city = $(this).val();
+        var properTimeFormat = "YYYY-MM-DDTHH:MM:ss";
+        startDate = moment($("#startDate").val()).format(properTimeFormat); // 2017-09-23T00:00:00-05:00
+        endDate = moment($("#startDate").val()).add(1, "day").format(properTimeFormat); // day after the start day
+
+        console.log(moment($("#startDate").val()).format(properTimeFormat));
 
         // This is our API key
         var APIKey = "VJQ763OD7ITTFKU7NOG4";
 
         // Here we are building the URL we need to query the database
-        var queryURL = "https://www.eventbriteapi.com/v3/events/search/?price=free&location.address=" + city + "&" + "start_date.range_start=" + startdate + "Z&start_date.range_end=" + enddate + "Z&token=" + APIKey;
+        var queryURL = "https://www.eventbriteapi.com/v3/events/search/?sort_by=date&categories=110,103&price=free&location.address=" + city + "&" + "start_date.range_start=" + startDate + "Z&start_date.range_end=" + endDate + "Z&token=" + APIKey;
 
         // We then created an AJAX call
         $.ajax({
@@ -25,54 +28,35 @@ $(document).ready(function() {
             // Create CODE HERE to Log the queryURL
             console.log(queryURL);
 
-    //-------------------------------------------------------------------- 
-            // Forrest's For Loop code
+            //tells us the length of event objects 
+            eventslength = response.events.length;
 
-            for (var i = 0; i < 10; i++) {
-                
+            $("#eventList").empty();
+
+            for (var i = 0; i < eventslength; i++) {
+
                 // Variables for what we get
                 var nameEvent = response.events[i].name.text;
                 var urlEvent = response.events[i].url;
                 var timeEvent = response.events[i].start.local;
+                var descriptionEvent = response.events[i].description.text
 
-                // Clean URL (something setting urlEvent to nameEvent)
-                var cleanUrl =
+                var date = moment(timeEvent).format("MMM Do");
+                var time = moment(timeEvent).format("h:mm a");
 
-                // Need to seperate the timeEvent into date and time using moment
-
-                console.log(nameEvent);
-                console.log(timeEvent);
-                console.log(urlEvent);
+                var weather = "<img id='weatherIcon' src='https://icons.wxug.com/i/c/a/partlycloudy.gif'>"
 
                 $("#eventList").append(
-                    "<tr><td>"
-                        + timeEvent +
-                    "</td><td>"
-                        + urlEvent +
-                    "</td><td>"
-                        + nameEvent +
-                    "</td>")
+                    '<tr><td>' + date +
+                    '</td><td>' + time + 
+                    '</td><td>' + weather +
+                    '</td><td><a target="-blank" href="' + urlEvent + '" data-toggle="tooltip" title="' + descriptionEvent + '">' +
+                    nameEvent +
+                    '</a></td>')
             }
 
-    //--------------------------------------------------------------------
-            // Create CODE HERE to log the resulting object
             console.log(response);
-
-            // for (var i = 0; i < response.length; i++) {
-
-            //     var datePath = "test";
-            //     var timePath = "test";
-            //     var namePath = "test";
-
-            //     $("#eventList").append(
-            //         '<hr>' +
-            //         '<div class=" row ">' +
-            //           '<div class="col-4 ">' + datePath + '</div>' +
-            //           '<div class="col-4 ">' + timePath + '</div>' +
-            //           '<div class="col-4 ">' + namePath + '</div>' +
-            //         '</div>'
-            //     );
-            // }
+           
         })
     };
 });
