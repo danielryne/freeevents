d5e434d2d9d9b6789b60e9ed4847dbbe3911b4d9
Daniel Lucio <Daniel@Daniels-MacBook-Air-2.local>
2017-09-23 11:49:39 -0500

    added weather API pull and icon

--- assets/logic.js
+++ assets/logic.js
@@ -10,30 +10,50 @@ $(document).ready(function() {
         var properTimeFormat = "YYYY-MM-DDTHH:MM:ss";
         startDate = moment($("#startDate").val()).format(properTimeFormat); // 2017-09-23T00:00:00-05:00
         endDate = moment($("#startDate").val()).add(1, "day").format(properTimeFormat); // day after the start day
-
         console.log(moment($("#startDate").val()).format(properTimeFormat));
 
-        // This is our API key
+        //setting our Weather API 
+        var weatherURL = 'http://api.wunderground.com/api/d2d1cb57c6d0b52c/conditions/q/TX/' + city +'.json';
+
+        //Create variables to hold the address for the weather icons
+        var weatherIcon = '';
+        var weatherurl = '';
+
+        //Calling the weather icon for the city
+        $.ajax({
+            url: weatherURL,
+            method: "GET"
+        }).done(function(response) {
+
+            // Logs the repsonse 
+            console.log(response);
+
+            weatherIcon = response.current_observation.icon_url;
+            weatherurl = response.current_observation.forecast_url;
+      
+        })
+
+        // API key for EventBrite
         var APIKey = "VJQ763OD7ITTFKU7NOG4";
 
-        // Here we are building the URL we need to query the database
+        // Query for Eventbrite 
         var queryURL = "https://www.eventbriteapi.com/v3/events/search/?sort_by=date&categories=110,103&price=free&location.address=" + city + "&" + "start_date.range_start=" + startDate + "Z&start_date.range_end=" + endDate + "Z&token=" + APIKey;
 
-        // We then created an AJAX call
+        // AJAX call 
         $.ajax({
             url: queryURL,
             method: "GET"
         }).done(function(response) {
 
-            // Create CODE HERE to Log the queryURL
-            console.log(queryURL);
-
+            // Logs the repsonse 
+            console.log(response);
 
             //tells us the length of event objects 
             eventslength = response.events.length;
 
             $("#eventList").empty();
 
+            //For loop to print the events 
             for (var i = 0; i < eventslength; i++) {
 
                 // Variables for what we get
@@ -45,19 +65,14 @@ $(document).ready(function() {
                 var date = moment(timeEvent).format("MMM Do");
                 var time = moment(timeEvent).format("h:mm a");
 
-                var weather = "<img id='weatherIcon' src='https://icons.wxug.com/i/c/a/partlycloudy.gif'>"
-
                 $("#eventList").append(
-                    '<tr><td>' + date +
-                    '</td><td>' + time + 
-                    '</td><td>' + weather +
+                    '<tr><td>' + date + 
+                    '</td><td>' + time +  
+                    '</td><td>' + '<a href="' + weatherurl + '" target="_blank"> <img src="' + weatherIcon + '"></a>' + 
                     '</td><td><a target="-blank" href="' + urlEvent + '" data-toggle="tooltip" title="' + descriptionEvent + '">' +
                     nameEvent +
                     '</a></td>')
-            }
-
-            console.log(response);
-           
+            }           
         })
     };
 });
